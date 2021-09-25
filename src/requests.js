1065067d3e6a994adc6becc2c15af52d9bb496d6
DrFamikon <guastavino.lambert@gmail.com>
2021-09-25 19:09:35 +0200

    flouflix

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "011c38cedc7092f1b50de7e89ccbe0aa";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=us-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=us-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
